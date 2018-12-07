44e491b2877a55f0b717eb22ec7de7a8a7094ef0
Fyodor <fyodor.work@gmail.com>
2018-12-07 11:41:25 +0200

    slider done

--- order-cabinet/src/components/carousel/carousel.js
+++ order-cabinet/src/components/carousel/carousel.js
@@ -8,74 +8,36 @@ import './carousel.sass';
 export default class Carousel extends Component {
   render() {
     return (
-      <section className="slider container">
-        {/* <div className="slider-cabinet new-cabinet">
-          <ul className="owl-carousel owl-theme order-slider">
-            <li className="item-slider">
-              <div className="icon" />
-              <div className="content">
-                <h2>День знаний!</h2>
-                <div className="text">
-                  <p>Слайд 1</p>
-                </div>
+      <section className="slider">
+        <OwlCarousel className="owl-theme" loop margin={0} items="1">
+          <div className="item">
+            <div className="container">
+              <img src="https://picsum.photos/200/300/?random" alt="lorem" />
+              <div className="text-wrapper">
+                <h4>Lorem, ipsum.</h4>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.</p>
               </div>
-            </li>
-            <li className="item-slider">
-              <div className="icon" />
-              <div className="content">
-                <h2>День знаний!</h2>
-                <div className="text">
-                  <p>Слайд 2</p>
-                </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="container">
+              <img src="https://picsum.photos/200/300/?random2" alt="lorem" />
+              <div className="text-wrapper">
+                <h4>Далеко-далеко, за.</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit iste iusto distinctio!</p>
               </div>
-            </li>
-            <li className="item-slider">
-              <div className="icon" />
-              <div className="content">
-                <h2>День знаний!</h2>
-                <div className="text">
-                  <p>Слайд 3</p>
-                </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="container">
+              <img src="https://picsum.photos/200/300/?random3" alt="lorem" />
+              <div className="text-wrapper">
+                <h4>Далеко-далеко за словесными.</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa dolor saepe iure aliquam esse.
+                </p>
               </div>
-            </li>
-          </ul>
-        </div> */}
-        <OwlCarousel className="owl-theme" loop margin={10} nav>
-          <div class="item">
-            <h4>1</h4>
-          </div>
-          <div class="item">
-            <h4>2</h4>
-          </div>
-          <div class="item">
-            <h4>3</h4>
-          </div>
-          <div class="item">
-            <h4>4</h4>
-          </div>
-          <div class="item">
-            <h4>5</h4>
-          </div>
-          <div class="item">
-            <h4>6</h4>
-          </div>
-          <div class="item">
-            <h4>7</h4>
-          </div>
-          <div class="item">
-            <h4>8</h4>
-          </div>
-          <div class="item">
-            <h4>9</h4>
-          </div>
-          <div class="item">
-            <h4>10</h4>
-          </div>
-          <div class="item">
-            <h4>11</h4>
-          </div>
-          <div class="item">
-            <h4>12</h4>
+            </div>
           </div>
         </OwlCarousel>
       </section>
