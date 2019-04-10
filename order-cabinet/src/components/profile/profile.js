f0d2bf201b6fd0281c5fd514df58b0312184d857
Fyodor <fyodor.work@gmail.com>
2019-04-10 12:23:09 +0300

    carousel back

--- order-cabinet/src/components/profile/profile.js
+++ order-cabinet/src/components/profile/profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 /*eslint-disable */
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 /*eslint-enable */
 import './profile.sass';
 
